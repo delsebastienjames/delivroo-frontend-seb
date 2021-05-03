829443aad58a757520259840e1108c250ba54b67
Sebastien <delsebastienjames@gmail.com>
2021-05-03 15:52:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  return <div className="container">Delivroo-seb</div>;
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3100/");
+    // console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
+  );
 };
 
 export default App;
