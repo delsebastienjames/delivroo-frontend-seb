a5509797d8bfe11e004da5d46f4c1c0915eb0c43
Sebastien <delsebastienjames@gmail.com>
2021-05-04 22:36:52 +0200

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,15 @@
 const Hero = ({ data }) => {
   return (
-    <div className="wrapper">
-      <div className="bred">
-        <div>
-          {" "}
-          <h2 className="data-name">{data.name}</h2>
-          <p className="data-description">{data.description}</p>
-        </div>
-
-        <div className="first-image">
-          <img src={data.picture} alt={data.name} />
+    <div className="montorgeuil">
+      <div className="wrapper">
+        <div className="bred">
+          <div>
+            <br />
+            <h2 className="data-name">{data.name}</h2>
+            <br />
+            <p className="data-description">{data.description}</p>
+          </div>
+          <img style={{ height: 200 }} src={data.picture} alt={data.name} />
         </div>
       </div>
     </div>
