116c48aa8a363d617609cd86b63255ea3de158ac
Sebastien <delsebastienjames@gmail.com>
2021-05-04 19:35:02 +0200

    Create Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,33 @@
+const Menu = ({ data }) => {
+  return (
+    <div>
+      {data.map((item) => {
+        return (
+          // si j'ai un id, je l'utilise
+          // sinon, je peux utiliser l'index
+          <div
+            key={item.id}
+            onClick={() => {
+              console.log(item.id);
+              // ajouter l'élément dans un tableau
+              // ce tableau sera parcouru pour afficher le panier
+            }}
+          >
+            <h4 className="item-title">{item.title}</h4>
+            <p className="item-description">{item.description}</p>
+            <span className="item-price">{item.price}</span>
+            {item.picture && (
+              <img
+                style={{ height: 150 }}
+                src={item.picture}
+                alt={item.title}
+              />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
