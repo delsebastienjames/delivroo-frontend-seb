ed3105ea26858d99e79c4a783dfc97c4e586a92d
Sebastien <delsebastienjames@gmail.com>
2021-05-05 08:16:44 +0200

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,18 +2,20 @@ import Menu from "./Menu";
 
 const Content = ({ data }) => {
   return (
-    <div className="wrapper">
-      {data.map((category, index) => {
-        return (
-          // if inline (if sans else)
-          category.meals.length > 0 && (
-            <div key={index}>
-              <h3 className="category-name">{category.name}</h3>
-              <Menu data={category.meals} />
-            </div>
-          )
-        );
-      })}
+    <div className="background-grey">
+      <div className="wrapper">
+        {data.map((category, index) => {
+          return (
+            // if inline (if sans else)
+            category.meals.length > 0 && (
+              <div key={index}>
+                <h3 className="category-name">{category.name}</h3>
+                <Menu data={category.meals} />
+              </div>
+            )
+          );
+        })}
+      </div>
     </div>
   );
 };
