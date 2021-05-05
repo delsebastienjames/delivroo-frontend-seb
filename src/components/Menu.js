07ae27cce6d5a08ea316f55a7f17ab4dbc2f6d41
Sebastien <delsebastienjames@gmail.com>
2021-05-05 16:36:10 +0200

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,19 @@
-const Menu = ({ data }) => {
+const Menu = ({ data, addToCart }) => {
   return (
-    <div>
+    <div className="menu-container">
       {data.map((item) => {
         return (
           // si j'ai un id, je l'utilise
           // sinon, je peux utiliser l'index
           <div
+            className="menu-item"
             key={item.id}
             onClick={() => {
-              console.log(item.id);
-              // ajouter l'élément dans un tableau
+              //console.log(item.id);
+              // console.log(item);
+              // ajouter l'élément dans un tableau cart (App.js)
               // ce tableau sera parcouru pour afficher le panier
+              addToCart(item);
             }}
           >
             <div className="bgc">
@@ -19,7 +22,6 @@ const Menu = ({ data }) => {
                   <h4 className="item-title">{item.title}</h4>
                   <p className="item-description">{item.description}</p>
                   <span className="item-price">{item.price}</span>
-
                   {item.picture && (
                     <img
                       style={{ height: 120 }}
